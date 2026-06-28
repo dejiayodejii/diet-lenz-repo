b25a0061cf00a61365de0ad4270309deae297bce
ayodejii <dejiayodeji010@gmail.com>
2026-06-28 21:33:26 +0100

    test fix

--- test/unit/app_config_test.dart
+++ test/unit/app_config_test.dart
@@ -3,25 +3,45 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('AppConfig', () {
-    test('defaults to development environment', () {
-      // dart-define is not set in test, so defaultValue applies
-      expect(AppConfig.environment, equals('development'));
+    test('throws when ENVIRONMENT is not supplied', () {
+      expect(
+        () => AppConfig.environment,
+        throwsA(
+          isA<StateError>().having(
+            (error) => error.message,
+            'message',
+            contains('ENVIRONMENT was not supplied'),
+          ),
+        ),
+      );
     });
 
-    test('isDevelopment is true when environment is development', () {
-      expect(AppConfig.isDevelopment, isTrue);
+    test('isDevelopment throws when ENVIRONMENT is not supplied', () {
+      expect(() => AppConfig.isDevelopment, throwsA(isA<StateError>()));
     });
 
-    test('isProduction is false when environment is development', () {
-      expect(AppConfig.isProduction, isFalse);
+    test('isProduction throws when ENVIRONMENT is not supplied', () {
+      expect(() => AppConfig.isProduction, throwsA(isA<StateError>()));
     });
 
-    test('apiUrl has a non-empty default', () {
-      expect(AppConfig.apiUrl, isNotEmpty);
+    test('throws when API_URL is not supplied', () {
+      expect(
+        () => AppConfig.apiUrl,
+        throwsA(
+          isA<StateError>().having(
+            (error) => error.message,
+            'message',
+            contains('API_URL was not supplied'),
+          ),
+        ),
+      );
     });
 
-    test('sentryTracesSampleRate is 1.0 in non-production', () {
-      expect(AppConfig.sentryTracesSampleRate, equals(1.0));
+    test('sentryTracesSampleRate throws when ENVIRONMENT is not supplied', () {
+      expect(
+        () => AppConfig.sentryTracesSampleRate,
+        throwsA(isA<StateError>()),
+      );
     });
   });
 }
